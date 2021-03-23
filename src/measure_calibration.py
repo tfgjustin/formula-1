9660774853b5106c6dad3c6198a8458978a0f242
justin <justin@tfgridiron.com>
2021-03-23 12:35:10 -0400

    Update calibration measuring/graphing program to print out cumulative reliability stats.

--- src/measure_calibration.py
+++ src/measure_calibration.py
@@ -27,6 +27,13 @@ _ODDS_WIN_IDX = _ELO_WIN_IDX + 1
 _CORRECT_WIN_IDX = _ODDS_WIN_IDX + 1
 _NUM_WIN_PARTS = _CORRECT_WIN_IDX + 1
 
+_TAG_FIN_IDX = 0
+_FIN_EVENT_IDX = _TAG_FIN_IDX + 1
+_FIN_MODE_IDX = _FIN_EVENT_IDX + 1
+_FIN_ODDS_IDX = _FIN_MODE_IDX + 1
+_FIN_CORRECT_IDX = _FIN_ODDS_IDX + 1
+_FIN_NUM_PARTS = _FIN_CORRECT_IDX + 1
+
 
 def get_h2h_predictions(filename, h2h_predictions):
     with open(filename, 'r') as infile:
@@ -43,17 +50,17 @@ def head_to_head(h2h_predictions):
     race_num = race_correct = race_exp = race_sse = 0
     qual_num = qual_correct = qual_exp = qual_sse = 0
     for parts in h2h_predictions:
-        confidence = float(parts[_CONFIDENCE_IDX])
+        confidence = float(parts[_CONFIDENCE_H2H_IDX])
         if confidence <= 0.5:
             continue
-        correct = int(parts[_CORRECT_IDX])
+        correct = int(parts[_CORRECT_H2H_IDX])
         diff = correct - confidence
-        if parts[_EVENT_IDX].endswith('Q'):
+        if parts[_EVENT_H2H_IDX].endswith('Q'):
             qual_num += 1
             qual_correct += correct
             qual_exp += confidence
             qual_sse += (diff * diff)
-        elif parts[_EVENT_IDX].endswith('R'):
+        elif parts[_EVENT_H2H_IDX].endswith('R'):
             race_num += 1
             race_correct += correct
             race_exp += confidence
@@ -64,16 +71,16 @@ def head_to_head(h2h_predictions):
     total_sse = race_sse + qual_sse
     if race_num:
         print('Races\t%7d\t%7d\t%.4f\t%.4f\t%.4f' % (race_num, race_correct,
-            float(race_correct) / race_num, race_exp / race_num,
-            race_sse / race_num))
+                                                     float(race_correct) / race_num, race_exp / race_num,
+                                                     race_sse / race_num))
     if qual_num:
-        print('Quali\t%7d\t%7d\t%.4f\t%.4f\t%.4f' % (qual_num, qual_correct,
-            float(qual_correct) / qual_num, qual_exp / qual_num,
-            qual_sse / qual_num))
+        print('Qual\t%7d\t%7d\t%.4f\t%.4f\t%.4f' % (qual_num, qual_correct,
+                                                    float(qual_correct) / qual_num, qual_exp / qual_num,
+                                                    qual_sse / qual_num))
     if total_num:
         print('Total\t%7d\t%7d\t%.4f\t%.4f\t%.4f' % (total_num, total_correct,
-            float(total_correct) / total_num, total_exp / total_num,
-            total_sse / total_num))
+                                                     float(total_correct) / total_num, total_exp / total_num,
+                                                     total_sse / total_num))
 
 
 def get_win_predictions(filename, win_predictions, event_type):
@@ -94,9 +101,9 @@ def get_win_predictions(filename, win_predictions, event_type):
 
 
 def one_win_calibration(tag, predictions, event_type):
+    _TARGET = 750
     print(len(predictions))
     n = 0
-    TARGET = 750
     odds_sum = 0
     correct_sum = 0
     bucket_num = 0
@@ -106,7 +113,7 @@ def one_win_calibration(tag, predictions, event_type):
         odds_sum += odds
         correct_sum += correct
         n += 1
-        if n == TARGET:
+        if n == _TARGET:
             expected_pct = odds_sum / n
             actual_pct = correct_sum / n
             error_pct = expected_pct - actual_pct
@@ -129,7 +136,53 @@ def one_win_calibration(tag, predictions, event_type):
 def win_calibration(win_predictions, event_type):
     for tag, predictions in win_predictions.items():
         one_win_calibration(tag, predictions, event_type)
-    return
+
+
+def get_finish_predictions(filename, mode, finish_predictions):
+    with open(filename, 'r') as infile:
+        for line in infile:
+            if not line.startswith('Finish\t'):
+                continue
+            parts = line.split('\t')
+            if len(parts) != _FIN_NUM_PARTS:
+                continue
+            if parts[_FIN_MODE_IDX] != mode:
+                continue
+            decade = int(parts[_FIN_EVENT_IDX][:3]) * 10
+            if decade not in finish_predictions:
+                finish_predictions[decade] = list()
+            finish_predictions[decade].append(parts)
+
+
+def print_finish_calibration(mode, all_predictions):
+    _TARGET = 50
+    for decade in (sorted(all_predictions.keys())):
+        predictions = all_predictions[decade]
+        print(len(predictions))
+        n = 0
+        odds_sum = 0
+        bucket_num = 0
+        correct_sum = 0
+        for row in sorted(predictions, key=lambda r: float(r[_FIN_ODDS_IDX])):
+            odds = float(row[_FIN_ODDS_IDX])
+            correct = int(row[_FIN_CORRECT_IDX])
+            odds_sum += odds
+            correct_sum += correct
+            n += 1
+            if n % _TARGET == 0:
+                expected_pct = odds_sum / n
+                actual_pct = correct_sum / n
+                error_pct = expected_pct - actual_pct
+                print('%s\t%d\t%4d\t%4d\t%4d\t%.4f\t%.4f\t%7.4f' % (
+                    mode, decade, n, odds_sum, correct_sum, expected_pct, actual_pct, error_pct)
+                      )
+        if n % _TARGET != 0:
+            expected_pct = odds_sum / n
+            actual_pct = correct_sum / n
+            error_pct = expected_pct - actual_pct
+            print('%s\t%d\t%4d\t%4d\t%4d\t%.4f\t%.4f\t%7.4f' % (
+                mode, decade, n, odds_sum, correct_sum, expected_pct, actual_pct, error_pct)
+                  )
 
 
 def main(argv):
@@ -139,10 +192,14 @@ def main(argv):
     # h2h_predictions = list()
     # get_h2h_predictions(argv[1], h2h_predictions)
     # head_to_head(h2h_predictions)
-    for event_type in ['Q', 'R']:
-        win_predictions = dict()
-        get_win_predictions(argv[1], win_predictions, event_type)
-        win_calibration(win_predictions, event_type)
+    # for event_type in ['Q', 'R']:
+    #     win_predictions = dict()
+    #     get_win_predictions(argv[1], win_predictions, event_type)
+    #     win_calibration(win_predictions, event_type)
+    for mode in ['All', 'Car', 'Driver']:
+        finish_predictions = dict()
+        get_finish_predictions(argv[1], mode, finish_predictions)
+        print_finish_calibration(mode, finish_predictions)
     sys.exit(0)
 
 
