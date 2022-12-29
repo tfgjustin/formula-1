c9f380fe3096ef1110c66708b691037086cee153
justin <justin@tfgridiron.com>
2022-12-29 14:36:43 -0500

    Update Driver class to keep track of the seasons in which they've taken part.

--- src/driver.py
+++ src/driver.py
@@ -1,10 +1,16 @@
+from collections import defaultdict
+
+
 class Driver(object):
+    _MIN_RACES_FOR_SEASON = 3
+
     def __init__(self, driver_id, driver_name, elo_rating, birthday=None, birth_year=None):
         self._id = driver_id
         self._name = driver_name
         self._rating = elo_rating
         self._birthday = birthday
-        self._birth_year = birth_year
+        self._birth_year = int(birth_year)
+        self._race_counts_by_year = defaultdict(int)
 
     def id(self):
         return self._id
@@ -17,6 +23,12 @@ class Driver(object):
 
     def birth_year(self):
         return self._birth_year
+
+    def seasons(self):
+        return sorted(
+            [year for year, count in self._race_counts_by_year.items() if count >= Driver._MIN_RACES_FOR_SEASON],
+            reverse=True
+        )
 
     def start_update(self, event_id, base_driver_reliability):
         if self._rating is None:
@@ -34,3 +46,9 @@ class Driver(object):
 
     def set_rating(self, rating):
         self._rating = rating
+
+    def add_year_participation(self, event_id):
+        if not event_id.endswith('R'):
+            return
+        year = int(event_id[0:4])
+        self._race_counts_by_year[year] += 1
